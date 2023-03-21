6eb79c374584d608e1db0aef228efe550b4520cb
ducuanh <127297089+ducuanh@users.noreply.github.com>
2023-03-21 18:07:43 +0100

    Create Lab2Zad41.py

---
+++ Lab2Zad41.py
@@ -0,0 +1,10 @@
+a = int(input("podaj 1 liczbe; "))
+b = int(input("podaj 2 liczbe; "))
+c = int(input("podaj 3 liczbe; "))
+
+delta = b**2 - 4*a*c
+
+if delta > 0:
+  print("Delta jest większa od 0")
+else:
+  print("Delta jest mniejsza od 0")
