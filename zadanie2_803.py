a2fa018c8c07438f7cc9d3648a495d4ed98ae770
ducuanh <127297089+ducuanh@users.noreply.github.com>
2023-03-21 18:17:05 +0100

    Create zadanie2_803.py

---
+++ zadanie2_803.py
@@ -0,0 +1,14 @@
+# Program do zbierania danych osobowych użytkownika s25002 DUC ANH DINH
+
+# Pobierz dane od użytkownika
+wiek = input("Podaj swój wiek: ")
+wzrost = input("Podaj swój wzrost (w cm): ")
+kolor_oczu = input("Podaj kolor swoich oczu: ")
+kolor_wlosow = input("Podaj kolor swoich włosów: ")
+
+# Wyświetl informacje
+print("Twoje dane:")
+print("Wiek:", wiek)
+print("Wzrost:", wzrost, "cm")
+print("Kolor oczu:", kolor_oczu)
+print("Kolor włosów:", kolor_wlosow)
